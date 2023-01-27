dbeef01c40ce32777a8bacebf62f064ee728447c
Lee Sang Ho <lee.sangho@startup-technology.com>
2023-01-27 14:21:21 +0000

    Add actix_web and sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+#[post("echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+async fn manual_hello() -> HttpResponse {
+    HttpResponse::Ok().body("Hey There")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("hey", web::get().to(manual_hello))
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
